e6df1461ebc037100f270187827341a12e39eb61
harshitvora <harshit.hvora@gmail.com>
2018-04-19 14:32:55 -0400

    updated setup description

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
             'cnp_chargeback_sdk_setup = scripts.cnp_chargeback_sdk_setup:main',
         ],
     },
-    long_description='''Vantiv eCommerce Python SDK
+    long_description='''Vantiv eCommerce Python Chargeback SDK
 =============================
 
 .. _`Vantiv eCommerce`: https://developer.vantiv.com/community/ecommerce
@@ -63,21 +63,15 @@ About Vantiv eCommerce
 
 About this SDK
 --------------
-The Vantiv eCommerce Python SDKt is a Python implementation of the `Vantiv eCommerce`_ XML API. This SDK was created to make it as easy as possible to connect to and process payments through Vantiv eCommerce. This SDK utilizes the HTTPS protocol to securely connect to Vantiv eCommerce.  Using the SDK requires coordination with the Vantiv eCommerce team to obtain credentials for accessing our systems.
+The Vantiv eCommerce Python Chargeback SDK is a Python implementation of the `Vantiv eCommerce`_ Chargeback API. This SDK was created to make it as easy as possible to manage your chargebacks using Vantiv eCommerce API. This SDK utilizes the HTTPS protocol to securely connect to Vantiv eCommerce. Using the SDK requires coordination with the Vantiv eCommerce team in order to be provided with credentials for accessing our systems.
 
-Each Python SDK release supports all of the functionality present in the associated Vantiv eCommerce XML version (e.g., 11.0.x supports Vantiv eCommerce XML v11.0). Please see the online copy of our XSD for Vantiv eCommerce XML to get more details on what the Vantiv eCommerce payments engine supports .
+Each Python SDK release supports all of the functionality present in the associated Vantiv eCommerce Chargeback API version (e.g., SDK v2.1.0 supports Vantiv eCommerce Chargeback API v2.1). Please see the Chargeback API reference guide to get more details on what the Vantiv eCommerce chargeback engine supports.
 
-This SDK was implemented to support the Python2 version 2.7.9 and later, Python3 version 3.4 and later, and was created by Vantiv eCommerce. Its intended use is for online transaction processing utilizing your account on the Vantiv eCommerce payments engine.
+This SDK was implemented to support the Python programming language and was created by Vantiv eCommerce. Its intended use is for online and batch transaction processing utilizing your account on the Vantiv eCommerce payments engine.
 
 See LICENSE file for details on using this software.
 
-Source Code available from : https://github.com/Vantiv/vantiv-sdk-for-python
-
-Examples can be found here https://github.com/Vantiv/vantiv-sdk-for-python/tree/master/samples
-
-Detail documents can be found here http://vantivecommercepythonsdk.readthedocs.io/en/latest/
-
-Please contact `Vantiv eCommerce`_  to receive valid merchant credentials and determine which version of the SDK is right for your business requirements or if you require assistance in any other way.  You can reach us at sdksupport@Vantiv.com
+Please contact `Vantiv eCommerce`_ to receive valid merchant credentials in order to run tests successfully or if you require assistance in any way.  We are reachable at sdksupport@Vantiv.com
 
 Dependencies
 ------------
@@ -89,11 +83,11 @@ Dependencies
 
 Setup
 -----
-* Run vantiv_python_sdk_setup and answer the questions.
+* Run cnp_chargeback_sdk_setup and answer the questions.
 
 .. code:: bash
 
-   vantiv_python_sdk_setup
+   cnp_chargeback_sdk_setup
 
 EXAMPLE
 -------
@@ -101,178 +95,33 @@ Using dict
 ..........
 .. code-block:: python
 
-    #Example for SDK
+    #Example for Chargeback SDK
     from __future__ import print_function, unicode_literals
-
+    
     from vantivsdk import *
-
-    # Initial Configuration object. If you have saved configuration in '.vantiv_python_sdk.conf' at system environment
-    # variable: VANTIV_SDK_CONFIG or user home directory, the saved configuration will be automatically load.
+    
+    # Initial Configuration object. If you have saved configuration in '.vantiv_chargeback_sdk.conf' at system environment
+    # variable: CHARGEBACK_SDK_CONFIG or user home directory, the saved configuration will be automatically load.
     conf = utils.Configuration()
-
-    # Configuration need following attributes for online request:
-    # attributes = default value
+    
+    # Configuration need following attributes for chargeback requests:
     # user = ''
     # password = ''
-    # merchant_id = ''
-    # reportGroup = 'Default Report Group'
-    # url = 'https://www.testlitle.com/sandbox/communicator/online'
+    # merchantId = ''
+    # url = 'https://www.testvantivcnp.com/sandbox/communicator/online'
     # proxy = ''
-    # print_xml = False
-
-    # Transaction presented by dict
-    txn_dict ={
-        'authorization':{
-            'orderId': '1',
-            'amount': 10010,
-            'orderSource': 'ecommerce',
-            'id': 'ThisIsRequiredby11',
-            'billToAddress': {
-                'name': 'John & Mary Smith',
-                'addressLine1': '1 Main St.',
-                'city': 'Burlington',
-                'state': 'MA',
-                'zip': '01803-3747',
-                'country': 'USA'
-            },
-            'card': {
-                'number': '4100000000000000',
-                'expDate': '1215',
-                'cardValidationNum' : '349',
-                'type': 'VI'
-            },
-            'enhancedData':{
-                'detailTax': [
-                    {'taxAmount':100},
-                    {'taxAmount':200},
-                ],
-            }
-        }
-    }
-
-    # Send request to server and get response as dict
-    response = online.request(txn_dict, conf)
-
-    print('Message: %s' % response['authorizationResponse']['message'])
-    print('LitleTransaction ID: %s' % response['authorizationResponse']['litleTxnId'])
-
-    # Configuration need following attributes for batch request:
-    # attributes = default value
-    # sftp_username = ''
-    # sftp_password = ''
-    # sftp_url = ''
-    # batch_requests_path = '/tmp/vantiv_sdk_batch_request'
-    # batch_response_path = '/tmp/vantiv_sdk_batch_response'
-    # fast_url = ''
-    # fast_ssl = True
-    # fast_port = ''
-    # id = ''
-
-    # Initial batch transactions container class
-    transactions = batch.Transactions()
-
-    # Add transaction to batch transactions container
-    transactions.add(txn_dict)
-
-    # Sent batch to server via socket and get response as dict
-    response = batch.stream(transactions, conf)
-
-    print('Message: %s' % response['batchResponse']['authorizationResponse']['message'])
-    print('LitleTransaction ID: %s' % response['batchResponse']['authorizationResponse']['litleTxnId'])
-
-Using object
-............
-.. code-block:: python
-
-    #Example for SDK
-    from __future__ import print_function, unicode_literals
-
-    from vantivsdk import *
-
-    # Initial Configuration object. If you have saved configuration in '.vantiv_python_sdk.conf' at system environment
-    # variable: VANTIV_SDK_CONFIG or user home directory, the saved configuration will be automatically load.
-    conf = utils.Configuration()
-
-    # Configuration need following attributes for online request:
-    # attributes = default value
-    # user = ''
-    # password = ''
-    # merchant_id = ''
-    # reportGroup = 'Default Report Group'
-    # url = 'https://www.testlitle.com/sandbox/communicator/online'
-    # proxy = ''
-    # print_xml = False
-
-    # Initial Transaction.
-    transaction = fields.authorization()
-    transaction.orderId = '1'
-    transaction.amount = 10010
-    transaction.orderSource = 'ecommerce'
-    transaction.id = 'ThisIsRequiredby11'
-
-    # Create contact object
-    contact = fields.contact()
-    contact.name = 'John & Mary Smith'
-    contact.addressLine1 = '1 Main St.'
-    contact.city = 'Burlington'
-    contact.state = 'MA'
-    contact.zip = '01803-3747'
-    contact.country = 'USA'
-    # The type of billToAddress is contact
-    transaction.billToAddress = contact
-
-    # Create cardType object
-    card = fields.cardType()
-    card.number = '4100000000000000'
-    card.expDate = '1215'
-    card.cardValidationNum = '349'
-    card.type = 'VI'
-    # The type of card is cardType
-    transaction.card = card
-
-    # detail tax
-    detailTaxList = list()
-
-    detailTax = fields.detailTax()
-    detailTax.taxAmount = 100
-    detailTaxList.append(detailTax)
-
-    detailTax2 = fields.detailTax()
-    detailTax2.taxAmount = 200
-    detailTaxList.append(detailTax2)
-
-    enhancedData = fields.enhancedData()
-    enhancedData.detailTax = detailTaxList
-
-    # Send request to server and get response as dict
-    response = online.request(transaction, conf)
-
-    print('Message: %s' % response['authorizationResponse']['message'])
-    print('LitleTransaction ID: %s' % response['authorizationResponse']['litleTxnId'])
-
-    # Configuration need following attributes for batch request:
-    # attributes = default value
-    # sftp_username = ''
-    # sftp_password = ''
-    # sftp_url = ''
-    # batch_requests_path = '/tmp/vantiv_sdk_batch_request'
-    # batch_response_path = '/tmp/vantiv_sdk_batch_response'
-    # fast_url = ''
-    # fast_ssl = True
-    # fast_port = ''
-    # id = ''
-
-    # Initial batch transactions container class
-    transactions = batch.Transactions()
-
-    # Add transaction to batch transactions container
-    transactions.add(transaction)
-
-    # Sent batch to server via socket and get response as dict
-    response = batch.stream(transactions, conf)
-
-    print('Message: %s' % response['batchResponse']['authorizationResponse']['message'])
-    print('LitleTransaction ID: %s' % response['batchResponse']['authorizationResponse']['litleTxnId'])
+    
+    # Retrieving information about a chargeback by caseId:
+    response = chargeback_retrieval.get_chargeback_by_case_id(xxxx)
+    response = chargeback_retrieval.get_chargebacks_by_date("2018-01-01)
+    
+    # Update chargeback case
+    chargeback_update.represent_case(xxxx, "Note on activity: represented case!")
+    chargeback_update.assign_case_to_user(xxxx, "userId", "Note on activity: assigned case to user!")
+    
+    # Upload and manage documents to support chargeback case
+    chargeback_docuemnt.upload_document(xxxx, "invoice.pdf")
+    response = chargeback_docuemnt.list_documents(xxxx)
 
 ''',
 )
